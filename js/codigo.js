f53b4641b2cc7aca64d4ca557789ac236726e5e7
Julieta Garcia <julieta.garcia96@gmail.com>
2021-02-02 14:15:09 -0300

    animaciones en minutos dias y horas

--- js/codigo.js
+++ js/codigo.js
@@ -1,9 +1,8 @@
 // Set the date we're counting down to
-let countDownDate = new Date("Feb 09, 2021 00:00:00").getTime();
+let countDownDate = new Date("Feb 4, 2021 00:00:00").getTime();
 
 // Update the count down every 1 second
 let x = setInterval(countDown, 1000);
-
 
 function countDown() {
 
@@ -24,17 +23,31 @@ function countDown() {
     document.getElementById("hours").innerHTML=hours;
     document.getElementById("minutes").innerHTML=minutes;
     document.getElementById("seconds").innerHTML=seconds;
+    
+    //Add "0"
+    if (seconds <10){
+      document.getElementById("seconds").innerHTML="0"+seconds
+    }
+    if (minutes<10){
+      document.getElementById("minutes").innerHTML="0"+minutes
+    }
+    if (hours <10){
+      document.getElementById("hours").innerHTML="0"+hours
+    }
   
-    // If the count down is finished, write some text
+    //If the count down is finished, write some text 
     if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("demo").innerHTML = "EXPIRED";
+       clearInterval(x); //NO FUNCIONA :'(
+       document.getElementById("main-title").innerHTML = "EXPIRED";
     }
     
   };
-  let y = setInterval(minutesAnimation, 1000);
-//Minutes Countdown Animation
-function minutesAnimation (){
+  
+// Minutes Hours Days Countdown Animation
+//set interval to run the minutes animation
+let y = setInterval(animation, 1000);
+
+function animation (){
   // Get today's date and time
   let now = new Date().getTime();
   
@@ -48,15 +61,46 @@ function minutesAnimation (){
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   seconds= parseInt(seconds);
+  minutes= parseInt(minutes);
+  hours= parseInt(hours);
   
+  // seconds animation
   let bgTopMinutes=document.getElementById("bg-top-minutes");
   let bgBottomMinutes=document.getElementById("bg-bottom-minutes");
-  if (seconds===0){
-    bgTopMinutes.classList.add('bg-top-minutes');  
-    bgBottomMinutes.classList.add('bg-bottom-minutes')
-  }else if(bgTopMinutes.classList.contains("bg-top-minutes")){
-    bgTopMinutes.classList.remove('bg-top-minutes');
-    bgBottomMinutes.classList.remove('bg-bottom-minutes');
-  }
-};
+  
+  // Add or remove the animation class
+  if (seconds===0 && minutes>0){
+    bgTopMinutes.classList.add('bg-top-animation');  
+    bgBottomMinutes.classList.add('bg-bottom-animation')
+  }else if(bgTopMinutes.classList.contains("bg-top-animation")){
+    bgTopMinutes.classList.remove('bg-top-animation');
+    bgBottomMinutes.classList.remove('bg-bottom-animation');
+  };
+  
+  // hours animation
+  let bgTopHours=document.getElementById("bg-top-hours");
+  let bgBottomHours=document.getElementById("bg-bottom-hours");
 
+  // Add or remove the animation class
+  if (minutes===0 && hours>0 && seconds===0){
+    bgTopHours.classList.add('bg-top-animation');  
+    bgBottomHours.classList.add('bg-bottom-animation')
+  }else if( bgTopHours.classList.contains("bg-top-animation")){
+    bgTopHours.classList.remove('bg-top-animation');
+    bgBottomHours.classList.remove('bg-bottom-animation');
+  };
+
+  // days animation
+  let bgTopDays=document.getElementById("bg-top-days");
+  let bgBottomDays=document.getElementById("bg-bottom-days");
+
+  // Add or remove the animation class
+  if (hours===0 && days>0 && minutes===0 &&seconds===0){
+    bgTopDays.classList.add('bg-top-animation');  
+    bgBottomDays.classList.add('bg-bottom-animation')
+  }else if( bgTopDays.classList.contains("bg-top-animation")){
+    bgTopDays.classList.remove('bg-top-animation');
+    bgBottomDays.classList.remove('bg-bottom-animation');
+  };
+
+}
